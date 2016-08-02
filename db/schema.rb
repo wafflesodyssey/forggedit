e4cb7066dc35fd27ac2b4db2ca8964599bcba701
tony un <tony un>
2016-08-02 14:27:04 -0400

    added procfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730234648) do
+ActiveRecord::Schema.define(version: 20160802143418) do
 
   create_table "links", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20160730234648) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  validates :email, format: {with: /@/}, uniqueness:true
   # test "the truth" do
   #   assert true
   # end
+
+
+
+  def test_can_set_a_password
+    user = User.new(email: 'example3@email.com')
+    user.password = "dc_ror_june_is_awesome"
+    assert_match /[A-Fa-f0-9]{64}/, user.password_digest
+  end
+
+  def test_can_check_password
+    user = User.new(email: 'example3@example.com')
+    user.password = "dc_ror_june_is_awesome"
+    assert user.password_digest == "dc_ror_june_is_awesome"
+    
+  end
 end
